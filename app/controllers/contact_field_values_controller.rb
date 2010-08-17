adb1a9656a25832a27647b4f3979333c62cb4e15
Erik M Jacobs <erikmjacobs@gmail.com>
2010-08-17 15:51:55 -0400

    Working on contact field values. Probably want contact field values to have a user id that corresponds to them.

--- app/controllers/contact_field_values_controller.rb
+++ app/controllers/contact_field_values_controller.rb
@@ -1,2 +1,41 @@
 class ContactFieldValuesController < ApplicationController
+
+  def new
+    @contact_field_value = ContactFieldValue.new
+
+    # grab the field that this value belongs to so we can display its name
+    @contact_field = ContactField.find(params[:field])
+  end
+
+  def create
+    # create a ContactFieldValue with the incoming params
+    @contact_field_value = ContactFieldValue.new :contact_id => params[:contact_id], :contact_field_id => params[:contact_field_id], :field_value => params[:contact_field_value][:field_value]
+
+    # before we do anything, we want to make sure that this value belongs to the user that is trying to create it
+    # find the field that corresponds with this value
+    @contact_field = ContactField.find(params[:contact_field_id])
+
+    # does it belong to the current user?
+    if @contact_field.user_id != current_user.id
+      flash[:warning] = "That field doesn't belong to you!"
+      redirect_to root_path
+    else
+      # the field is ours, so we will try to save it
+      if @contact_field_value.save
+        flash[:success] = "Value added!"
+        redirect_to contacts_path
+      else
+        flash[:error] = "Something didn't work right.  Try again."
+        render :action => 'new'
+      end
+    end
+
+  end
+
+  def update
+  end
+
+  def delete
+  end
+
 end
--- app/helpers/contacts_helper.rb
+++ app/helpers/contacts_helper.rb
@@ -1,10 +1,11 @@
 module ContactsHelper
 
   def field_value_output_link(field, contact)
-    logger.info "#{contact.name} - #{field.id}"
     value = ContactFieldValue.find_value_from_field_and_contact(field.id, contact.id).first
     if value.nil?
       link_to "Create", new_contact_field_value_path( :contact => contact.id, :field => field.id )
+    else
+      return "#{value.field_value.to_s} [#{link_to "x", value, :method => 'delete', :confirm => "Really delete this value?"}]"
     end
   end
 
